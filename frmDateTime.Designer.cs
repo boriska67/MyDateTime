17b4754e16cef64ebc35740eb5a790d13daa6afd
boriska67 <borik_k@hotmail.com>
2020-11-04 14:42:52 -0800

    Initial commit

---
+++ frmDateTime.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MyDateTime
+{
+    partial class frmDateTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowDateTime = new System.Windows.Forms.Button();
+            this.btnWriteDateTimeToFile = new System.Windows.Forms.Button();
+            this.btnReadDateTimeFRomFile = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.openFileDialogDateTime = new System.Windows.Forms.OpenFileDialog();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnShowDateTime
+            // 
+            this.btnShowDateTime.Location = new System.Drawing.Point(21, 29);
+            this.btnShowDateTime.Name = "btnShowDateTime";
+            this.btnShowDateTime.Size = new System.Drawing.Size(141, 26);
+            this.btnShowDateTime.TabIndex = 0;
+            this.btnShowDateTime.Text = "Show Date Time";
+            this.btnShowDateTime.UseVisualStyleBackColor = true;
+            this.btnShowDateTime.Click += new System.EventHandler(this.btnShowDateTime_Click);
+            // 
+            // btnWriteDateTimeToFile
+            // 
+            this.btnWriteDateTimeToFile.Location = new System.Drawing.Point(21, 85);
+            this.btnWriteDateTimeToFile.Name = "btnWriteDateTimeToFile";
+            this.btnWriteDateTimeToFile.Size = new System.Drawing.Size(141, 26);
+            this.btnWriteDateTimeToFile.TabIndex = 1;
+            this.btnWriteDateTimeToFile.Text = "Write Date Time to File";
+            this.btnWriteDateTimeToFile.UseVisualStyleBackColor = true;
+            this.btnWriteDateTimeToFile.Click += new System.EventHandler(this.btnWriteDateTimeToFile_Click);
+            // 
+            // btnReadDateTimeFRomFile
+            // 
+            this.btnReadDateTimeFRomFile.Location = new System.Drawing.Point(21, 145);
+            this.btnReadDateTimeFRomFile.Name = "btnReadDateTimeFRomFile";
+            this.btnReadDateTimeFRomFile.Size = new System.Drawing.Size(141, 26);
+            this.btnReadDateTimeFRomFile.TabIndex = 2;
+            this.btnReadDateTimeFRomFile.Text = "Read Date Time from File";
+            this.btnReadDateTimeFRomFile.UseVisualStyleBackColor = true;
+            this.btnReadDateTimeFRomFile.Click += new System.EventHandler(this.btnReadDateTimeFRomFile_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(268, 145);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(141, 26);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // openFileDialogDateTime
+            // 
+            this.openFileDialogDateTime.FileName = "openFileDialog1";
+            // 
+            // lblTime
+            // 
+            this.lblTime.Location = new System.Drawing.Point(265, 29);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(144, 26);
+            this.lblTime.TabIndex = 4;
+            // 
+            // frmDateTime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 194);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReadDateTimeFRomFile);
+            this.Controls.Add(this.btnWriteDateTimeToFile);
+            this.Controls.Add(this.btnShowDateTime);
+            this.MaximizeBox = false;
+            this.Name = "frmDateTime";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Date Time";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowDateTime;
+        private System.Windows.Forms.Button btnWriteDateTimeToFile;
+        private System.Windows.Forms.Button btnReadDateTimeFRomFile;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.OpenFileDialog openFileDialogDateTime;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ frmDateTime.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MyDateTime
+{
+    public partial class frmDateTime : Form
+    {
+        public frmDateTime()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnShowDateTime_Click(object sender, EventArgs e)
+        {
+            lblTime.Text = string.Empty;
+            lblTime.Text = Time();
+            String output = Time();
+
+            DateTime dt = DateTime.ParseExact(output, String.Format("yyyy-dd-MM HH:mm:ss"), null);
+        }
+
+        private void btnWriteDateTimeToFile_Click(object sender, EventArgs e)
+        {
+            string time = Time();
+
+            using (SaveFileDialog save = new SaveFileDialog())
+            {
+                save.FileName = "test.txt";
+                save.InitialDirectory = "c:\\test\\readwrite";
+                save.Filter = "txt files (*.txt)|*.txt|log files (*.log)|*.log";
+                save.OverwritePrompt = false;
+
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter writer = new StreamWriter(save.OpenFile());
+
+                    try
+                    {
+                        writer.WriteLine(time);
+                        writer.Dispose();
+                        writer.Close();
+
+                        lblTime.Text = string.Empty;
+                        lblTime.Text = "Success!";
+                    }
+                    catch (IOException z)
+                    {
+                        lblTime.Text = string.Empty;
+                        lblTime.Text = z.ToString();// "Error! Unable to write into the files!";
+                        writer.Dispose();
+                        writer.Close();
+                    }
+                }
+            }
+        }
+
+        private void btnReadDateTimeFRomFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialogDateTime = new OpenFileDialog())
+            {
+                string fileContent = string.Empty;
+                string filePath = string.Empty;
+
+                openFileDialogDateTime.InitialDirectory = "c:\\test\\readwrite";
+                openFileDialogDateTime.RestoreDirectory = true;
+                openFileDialogDateTime.Filter = "txt files (*.txt)|*.txt|log files (*.log)|*.log";
+                openFileDialogDateTime.FilterIndex = 1;
+
+                if (openFileDialogDateTime.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialogDateTime.FileName;
+                    Stream fileStream = openFileDialogDateTime.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+
+                    fileStream.Dispose();
+                    fileStream.Close();
+                }
+
+                lblTime.Text = string.Empty;
+                lblTime.Text = fileContent;
+            } 
+        }
+
+        private string Time()
+        {
+            DateTime savedNow = DateTime.Now;
+            return String.Format("{0:yyyy-dd-MM}", savedNow) + " " + 
+                String.Format("{0:00}", DateTime.Parse(savedNow.ToLongTimeString()).TimeOfDay.Hours) + ":" + 
+                String.Format("{0:00}", DateTime.Parse(savedNow.ToLongTimeString()).TimeOfDay.Minutes) + ":" + 
+                String.Format("{0:00}", DateTime.Parse(savedNow.ToLongTimeString()).TimeOfDay.Seconds);
+        }
+    }
+}
